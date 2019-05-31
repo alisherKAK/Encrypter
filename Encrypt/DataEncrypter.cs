de92789a59caf52beb5c7c353045f81cb7db57c0
AlisherKaal <alisherkala007@gmail.com>
2019-05-31 11:53:41 +0600

    Encrypter 31.05.19

---
+++ Encrypt/DataEncrypter.cs
@@ -0,0 +1,17 @@
+﻿using DevOne.Security.Cryptography.BCrypt;
+
+namespace Encrypt
+{
+    public class DataEncrypter
+    {
+        public static string HashString(string text)
+        {
+            return BCryptHelper.HashPassword(text, BCryptHelper.GenerateSalt());
+        }
+
+        public static bool CheckString(string condidate, string hashedString)
+        {
+            return BCryptHelper.CheckPassword(condidate, hashedString);
+        }
+    }
+}
---
+++ Encrypt/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string:");
+
+            string text = Console.ReadLine();
+
+            using (var file = File.CreateText("Hashed Text.txt"))
+            {
+                file.WriteLine(DataEncrypter.HashString(text) + " - " + text);
+            }
+        }
+    }
+}
